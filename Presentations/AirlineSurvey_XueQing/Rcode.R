db66ec0f1fd9295840dd4798c051ec419968ba4f
Hui Lin <longqiman@gmail.com>
2016-07-19 22:21:36 -0500

    talk

--- Presentations/AirlineSurvey_XueQing/Rcode.R
+++ Presentations/AirlineSurvey_XueQing/Rcode.R
@@ -11,7 +11,7 @@ library(RColorBrewer)
 airline<-read.csv("https://raw.githubusercontent.com/happyrabbit/DataScientistR/master/Data/AirlineRating.csv")
 # airline<-read.csv("/Users/happyrabbit/Documents/GitHub/DataScientistR/Data/AirlineRating.csv")
 glimpse(airline)
-
+#######-----------------------------------------------
 # 我们用`corrplot()`函数检查问卷调查问题的相关性：
 
 # 选取其中的问卷调查项
@@ -21,7 +21,19 @@ select(airline,Easy_Reservation:Recommend)%>%
   # 用corrplot()绘制相关图
   # 选项order="hclust"按照变量的相似度，基于系统聚类的结果对行列进行重新排列
   corrplot(,order="hclust")
+#######-----------------------------------------------
+# 主成分分析
+airline.pc<-select(airline,Easy_Reservation:Recommend)%>%
+  prcomp()
+summary(airline.pc)
 
+# 主成分分析陡坡图
+plot(airline.pc,type="l",family ="Songti SC",main="PCA陡坡图")
+
+# PCA双标图
+biplot(airline.pc,family ="Songti SC",main="PCA双标图",cex=c(0.5,1),xlim=c(-0.06,0.04))
+
+#######-----------------------------------------------
 # 我们可以用之前介绍的`dplyr`包中的各种函数，以及使用之前讲到的管道操作`%>%`让代码更易读：
 # 选取其中的问卷调查项和航空公司因子信息
 # 即删除ID项
@@ -33,6 +45,13 @@ airline.mean<-select(airline,-ID)%>%
   # 显示数据
   glimpse()
 
+# 聚合后PCA结果双标图
+airline.mean.pc<-select(airline.mean,Easy_Reservation:Recommend)%>%
+  prcomp()
+biplot(airline.mean.pc,family ="Songti SC",main="聚合后PCA结果双标图",
+       cex=0.7, expand=2,xlim=c(-0.8, 1),ylim=c(-0.7,0.8))
+
+#######-----------------------------------------------
 # 将航空公司设置成行名称然后将对应的字符列删除
 row.names(airline.mean)<-airline.mean$Airline
 airline.mean<-select(airline.mean,-Airline)
@@ -41,25 +60,6 @@ heatmap.2(as.matrix(airline.mean),
           col=brewer.pal(9,"YlGn"),trace="none",key=FALSE,dend="none",cexCol=0.6,cexRow =1)
 title(family ="Songti SC",
       main="航空公司问卷调查均值热图")
-
-# 主成分分析
-airline.pc<-select(airline,Easy_Reservation:Recommend)%>%
-  prcomp()
-summary(airline.pc)
-
-
-# 主成分分析陡坡图
-plot(airline.pc,type="l",family ="Songti SC",main="PCA陡坡图")
-
-# PCA双标图
-biplot(airline.pc,family ="Songti SC",main="PCA双标图",cex=c(0.5,1),xlim=c(-0.06,0.04))
-
-# 聚合后PCA结果双标图
-airline.mean.pc<-select(airline.mean,Easy_Reservation:Recommend)%>%
-  prcomp()
-biplot(airline.mean.pc,family ="Songti SC",main="聚合后PCA结果双标图",
-       cex=0.7, expand=2,xlim=c(-0.8, 1),ylim=c(-0.7,0.8))
-
 
 # 因子分析
 library(GPArotation)
@@ -74,8 +74,6 @@ heatmap.2(airline.fa$loadings,
           col=brewer.pal(9,"YlGn"),trace="none",key=FALSE,dend="none",cexCol=0.6,cexRow =1)
 title(family ="Songti SC",
       main="航空公司满意度因子载荷")
-
-library(semPlot)
 
 # 因子得分
 airline.fa<-airline%>%
