2c02bc48b1fee813313d45eafc2a21b80e6f1fd8
Hui Lin <longqiman@gmail.com>
2017-03-31 13:21:07 -0500

    reading

--- CodeBookList.R
+++ CodeBookList.R
@@ -13,9 +13,26 @@ write.csv(dat,"/Users/happyrabbit/Documents/GitHub/hui1987.com/ReadingListHui.cs
 ###########
 library(readr)
 library(dplyr)
+library(tibble)
 dat<-read.csv("/Users/happyrabbit/Documents/GitHub/hui1987.com/ReadingListHui.csv",stringsAsFactors = F)
 
-library(tibble)
+# combine "Chinese.Version","English.Version","Audio" together
+c1<-toupper(dat$Chinese.Version)
+table(c1)
+c1<-paste("CN",c1,sep="_")
+c1[which(c1=="CN_")]<-""
+###########
+c2<-toupper(dat$English.Version)
+table(c2)
+c2<-paste("EN",c2,sep="_")
+c2[which(c2=="EN_")]<-""
+c2[which(c2=="EN_NO ")]<-"EN_NO"
+###########
+c0<-paste(c1,c2,sep=" & ")
+
+table(c0)
+summary(dat)
+
 dat<-as_tibble(dat)
 ######## Adding new book
 x1<- tibble("艺用人体结构运动学",0,90,"P",NA,"someone","Art",'ART')
